983300bf108a8d2823e84a508a774e476ee2d51d
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-09-30 13:00:31 +0530

    class1 what is variable

---
+++ frontend/JavaScript/class-1/index.js
@@ -0,0 +1,11 @@
+console.log("jay jagannath");
+console.log("i'm Bikash");
+a = 20;
+b = 30;
+console.log(a);
+console.log(age);
+// console.log(place); error because it is defined with const keyword so we can't use it before declearation
+var age = 20;
+let name = "bhagya";
+const place = "kendrapara";
+console.log(name, age, place);
