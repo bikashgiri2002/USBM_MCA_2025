96bc527cded1b8f5e041c917b4b8d4a37fe10243
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-10-01 11:55:06 +0530

    class 2 type of data in js and hosting in js

---
+++ frontend/JavaScript/class-2/index.js
@@ -0,0 +1,18 @@
+console.log("class 2");
+console.log("01-10-2024");
+console.log(typeof(null));
+console.log(typeof(undefined));
+//js use 64bit representation for number data type 
+//so it has maximun range ie 9007199254740992
+//and for decimal it is upto 15 to 16 decimal range
+a();
+var a = 10;
+function a() {
+    console.log("its a a functon");
+}
+//a()
+//its shows a error because function hosted to the top
+//var implementation
+console.log(address);
+var address = "bhubaneswar";
+console.log(address);
---
+++ frontend/JavaScript/class-2/note.md
@@ -0,0 +1,3 @@
+## js use 64bit representation for number data type 
+## so it has maximun range ie 9007199254740992
+## and for decimal it is upto 15 to 16 decimal range
