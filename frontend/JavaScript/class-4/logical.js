675e4a9af1e59f75c7be52411984e891c8607b80
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-10-08 12:14:09 +0530

    loginal operator

---
+++ frontend/JavaScript/class-4/logical.js
@@ -0,0 +1,7 @@
+// &(and), ||(or), !(not) 
+let ch = 'b';
+if (ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u') {
+    console.log("it's a vowel");
+}else{
+    console.log("it's a consonant");
+}
