50f51889241e7a24aa855ed0c1984eda3be15d9e
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-10-08 12:04:45 +0530

    conditioanl operator

--- frontend/JavaScript/class-4/coditional.js
+++ frontend/JavaScript/class-4/coditional.js
@@ -1,5 +1,6 @@
 //conditional statement
 // false values are : false, 0, -0, "", '', ``, null, undefine, NaN
+console.log("task 1");
 console.log("discount start if toatl bill is more than 999 you get 10% ");
 let totalBill = 999;
 console.log(`total bill = ${totalBill}`);
@@ -7,6 +8,18 @@ if(totalBill >= 999) {
     totalBill = totalBill - totalBill * 0.1;
 }
 console.log(`final bill = ${totalBill}`);
+console.log("\n");
+console.log("task 2");
+let a = 60;
+if(a >= 60) {
+    console.log("Senior citizens");
+}else if(a >= 18) {
+    console.log("adult");
+}else{
+    console.log("teen");
+}
+console.log("\n");
+console.log("task 3");
 let gender = "male";
 let age = 22;
 if(gender == "male") {
